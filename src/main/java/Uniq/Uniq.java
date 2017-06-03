e3b381ae9a63ab6822a2ac5cf2441870100e75d9
Александра Суменко <aleksa.sumenko@yandex.ru>
2017-06-04 00:00:04 +0300

    update

--- src/main/java/Uniq/Uniq.java
+++ src/main/java/Uniq/Uniq.java
@@ -2,76 +2,68 @@ package main.java.Uniq;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 @SuppressWarnings("WeakerAccess")
 public final class Uniq {
 
-    private String inputFileName;
-    private String outputFileName;
     private boolean insensitiveCase;
     private int ignoreNSymbols;
     private boolean uniqueStrings;
     private boolean numberOfEqualStrings;
 
-    public Uniq(String inputFile, String outputFile, boolean insensitiveCase,
-                int ignoredNSymbols, boolean uniqueStrings, boolean numberOfEqualStrings) {
-
-        this.inputFileName = inputFileName;
-        this.outputFileName = outputFileName;
+   public Uniq(boolean insensitiveCase, int ignoreNSymbols, boolean uniqueStrings, boolean numberOfEqualStrings) {
         this.insensitiveCase = insensitiveCase;
-        this.ignoreNSymbols = ignoredNSymbols;
+        this.ignoreNSymbols = ignoreNSymbols;
         this.uniqueStrings = uniqueStrings;
         this.numberOfEqualStrings = numberOfEqualStrings;
     }
 
-    public void uniq(InputStream in, OutputStream out) throws IOException {
-        PrintWriter output = new PrintWriter(out);
-        ArrayList<String> strings = new ArrayList<>();
+    public ArrayList<String> uniq(Reader in) throws IOException {
         ArrayList<String> result = new ArrayList<>();
-        try (Scanner scanner = new Scanner(in)) {
-            String str;
-            while (scanner.hasNextLine()) {
-                str = scanner.nextLine();
-                strings.add(str);
+        BufferedReader br = new BufferedReader(in);
+        int equal = 0;
+        int num = 1;
+        String str1;
+        String str2 = br.readLine();
+        String a;
+        while (((a = br.readLine()) != null)) {
+            str1 = str2;
+            str2 = a;
+            equal = 0;
+            if (str1.equals(str2))
+                equal++;
+            if (insensitiveCase) {
+                if (str1.equalsIgnoreCase(str2))
+                    equal++;
             }
-            if ((!uniqueStrings) && (!numberOfEqualStrings)) {
-                result.addAll(strings);
+            if (ignoreNSymbols > 0) {
+                if (str1.length() > ignoreNSymbols)
+                    str1 = str1.substring(ignoreNSymbols);
+                if (str2.length() > ignoreNSymbols)
+                    str2 = str2.substring(ignoreNSymbols);
+                if (str1.equals(str2))
+                    equal++;
+            }
+            if (equal != 0) {
+                num++;
             } else {
-                strings.add("0");
-                int[] numEqual = new int[strings.size()];
-                numEqual[strings.size() - 1] = 1;
-                for (int i = 0; i < strings.size() - 1; i++) {
-                    numEqual[i] = 1;
-                    String str1 = strings.get(i);
-                    String str2 = strings.get(i + 1);
-                    int comparison = 0;
-                    if (insensitiveCase) {
-                        if (str1.equalsIgnoreCase(str2))
-                            comparison++;
-                    }
-                    if (ignoreNSymbols > 0) {
-                        str1 = str1.substring(ignoreNSymbols);
-                        str2 = str2.substring(ignoreNSymbols);
-                        if (str1.equals(str2))
-                            comparison++;
-                    }
-                    if (uniqueStrings) {
-                        if (str1.equals(str2))
-                            comparison++;
-                    }
-                    if (comparison > 0) {
-                        numEqual[i + 1] = numEqual[i] + 1;
-                    } else {
-                        if (numberOfEqualStrings) {
-                            str1 = numEqual[i] + str1;
-                        }
-                        result.add(str1);
-                        output.println(result.get(i));
-                    }
-                }
+                add(str1, num, result);
+                num = 1;
             }
         }
-        output.close();
+        add(str2, num, result);
+
+        return result;
+    }
+
+    private void add(String str, int num, ArrayList<String> result) {
+        if ((uniqueStrings) && (num == 1)) {
+            result.add(str);
+        }
+        if (!uniqueStrings) {
+            if (numberOfEqualStrings) {
+                result.add(num + " " + str);
+            } else result.add(str);
+        }
     }
 }
--- src/main/java/Uniq/UniqLauncher.java
+++ src/main/java/Uniq/UniqLauncher.java
@@ -8,23 +8,23 @@ import org.kohsuke.args4j.Option;
 import java.io.*;
 
 public class UniqLauncher {
-    @Argument(metaVar = "file", usage = "Input file name")
-    private String inputFileName;
-
-    @Option(name = "-o", metaVar = "ofile", usage = "Output file name")
-    private String outputFileName;
-
     @Option(name = "-i", metaVar = "insensetiveCase", usage = "Insensitive case")
-    private boolean insensetiveCase;
+    private boolean insensetiveCase = false;
 
     @Option(name = "-s", metaVar = "IgnoreNSymbols", usage = "N symbols ignoring")
-    private int ignoreNSymbols;
+    private int ignoreNSymbols = 0;
 
     @Option(name = "-u", metaVar = "uniqueStrings", usage = "Unique strings")
-    private boolean uniqueStrings;
+    private boolean uniqueStrings = false;
 
     @Option(name = "-c", metaVar = "numberOfEqualStrings", usage = "Number of equal strings")
-    private boolean numberOfEqualStrings;
+    private boolean numberOfEqualStrings = false;
+
+    @Option(name = "-o", metaVar = "ofile", usage = "Output file name")
+    private String outputFileName = null;
+
+    @Argument(metaVar = "file", usage = "Input file name")
+    private String inputFileName = null;
 
     public static void main(String[] args) {
         new UniqLauncher().launch(args);
@@ -37,25 +37,40 @@ public class UniqLauncher {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("java -jar Uniq.jar -i -u -c -s num -o ofile file");
+            System.err.println("java -jar Uniq.jar [-i] [-u] [-c] [-s num] [-o ofile] [file]");
             parser.printUsage(System.err);
             return;
         }
-        Uniq uniq = new Uniq(inputFileName, outputFileName, insensetiveCase, ignoreNSymbols, uniqueStrings, numberOfEqualStrings);
+
+       Uniq uniq = new Uniq(insensetiveCase, ignoreNSymbols, uniqueStrings, numberOfEqualStrings);
         try {
-            InputStream in;
-            OutputStream out;
+            Reader input;
             if (inputFileName != null) {
-                in = new FileInputStream(inputFileName);
+                input = new FileReader(inputFileName);
             } else {
-                in = new BufferedInputStream(System.in);
+                input = new BufferedReader(new InputStreamReader(System.in));
             }
+
+            Writer output;
             if (outputFileName != null) {
-                out = new FileOutputStream(outputFileName);
+                try {
+                    output = new FileWriter(outputFileName);
+                    int k = 0;
+                    for (String str : uniq.uniq(input)) {
+                        output.write(str);
+                        if (k < uniq.uniq(input).size())
+                            output.write("\n");
+                    }
+                    output.close();
+                } catch (IOException e) {
+                    System.out.println("Error writing file");
+                }
             } else {
-                out = new BufferedOutputStream(System.out);
+                for (String str : uniq.uniq(input)) {
+                    System.out.println(str);
+                }
             }
-            uniq.uniq(in, out);
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
